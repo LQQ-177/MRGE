678769e781a7eb6416a68c1d694d1011e436fa8c
LQQ-177 <65841147+LQQ-177@users.noreply.github.com>
2021-12-28 20:55:19 +0800

    Add files via upload

--- models/MRGE_bert.py
+++ models/MRGE_bert.py
@@ -21,7 +21,7 @@ class MRGE_bert(nn.Module):
         self.use_coreference = True
         self.use_distance = True
 
-        hidden_size = 128
+        self.hidden_size = 128
         #hidden_size = 768#bert_large
         bert_hidden_size = 768
         #bert_hidden_size = 1024#bert_large
@@ -44,19 +44,13 @@ class MRGE_bert(nn.Module):
         self.linear_doc = nn.Linear(bert_hidden_size+config.coref_size+config.entity_type_size, config.relation_num)
 
         self.tree_RNN = DepST_RNN(bert_hidden_size+config.coref_size+config.entity_type_size, config.dep_size, config.dep_rel_num)
-        #定义LSTM
+        self.tree_LSTM = EncoderLSTM(bert_hidden_size+config.coref_size+config.entity_type_size+config.dep_size, self.hidden_size, 1, True, False, 1 - config.keep_prob, False)
 
-        self.linear_temp = nn.Linear(bert_hidden_size+config.coref_size+config.entity_type_size+config.dep_size, hidden_size)
-
-        if self.use_distance:
-            self.dis_embed = nn.Embedding(20, config.dis_size, padding_idx=10)
-            self.linear_ent = nn.Linear(hidden_size+config.dis_size, config.relation_num)
-        else:
-            self.linear_ent = nn.Linear(hidden_size, config.relation_num)
+        self.linear_ent = nn.Linear(self.hidden_size, config.relation_num)
 
 
     def forward(self, context_idxs, pos, context_ner, context_char_idxs, context_lens, h_mapping, t_mapping,
-                relation_mask, dis_h_2_t, dis_t_2_h, sent_idxs, sent_lengths, reverse_sent_idxs, context_masks, context_starts, context_tree):
+                relation_mask, dis_h_2_t, dis_t_2_h, sent_idxs, sent_lengths, reverse_sent_idxs, context_masks, context_starts, context_tree, context_SDP, context_SDP_len):
         # para_size, char_size, bsz = context_idxs.size(1), context_char_idxs.size(2), context_idxs.size(0)
         # context_ch = self.char_emb(context_char_idxs.contiguous().view(-1, char_size)).view(bsz * para_size, char_size, -1)
         # context_ch = self.char_cnn(context_ch.permute(0, 2, 1).contiguous()).max(dim=-1)[0].view(bsz, para_size, -1)
@@ -94,27 +88,41 @@ class MRGE_bert(nn.Module):
         doc = self.linear_doc(doc)#(batch_size,97)
         #print(doc.size())
 
-        ent = self.tree_RNN(context_output, context_tree)
-        #调用LSTM
+        ent = self.tree_RNN(context_output, context_tree)#(batch_size,doc_len,1108)
+        #print(ent.size())
 
-        context_output = self.linear_temp(ent)#(batch_size,doc_len,128)
+        #print(context_SDP.size())#(batch_size,pre_num,2,20)
+        #print(context_SDP_len.size())#(batch_size,pre_num,2)
+        ent_head_output = torch.zeros(context_SDP.size(0), context_SDP.size(1), self.hidden_size).cuda()#(batch_size,pre_num,128)
+        ent_tail_output = torch.zeros(context_SDP.size(0), context_SDP.size(1), self.hidden_size).cuda()
+        #print(ent_head_output.size())
 
-        #print(h_mapping.size())#(batch_size,re_num,doc_len)
-        start_e_output = torch.matmul(h_mapping, context_output)#(batch_size,re_num,128)
-        end_e_output = torch.matmul(t_mapping, context_output)
+        for batch in range(ent.size(0)):#分文档处理
+            len = int(sum(context_SDP_len[batch, :, 0] != 0))#待处理关系个数
+            #print(len)
 
-        if self.use_distance:
-            s_ep = torch.cat([start_e_output, self.dis_embed(dis_h_2_t)], dim=-1)#(batch_size,re_num,148)
-            t_ep = torch.cat([end_e_output, self.dis_embed(dis_t_2_h)], dim=-1)
-            ep = s_ep - t_ep
-            ent = self.linear_ent(ep)#(batch_size,re_num,97)
-        else:
-            ep = start_e_output - end_e_output#(batch_size,doc_len,128)
-            ent = self.linear_ent(ep)
+            ent_head = torch.zeros(len, self.config.SDP_deep, ent.size(2)).cuda()#(len,20,1108)
+            ent_tail = torch.zeros(len, self.config.SDP_deep, ent.size(2)).cuda()
+            #print(ent_head.size())
+
+            for pre_num in range(len):#分待预测关系处理
+                ent_head[pre_num] = torch.index_select(ent[batch], 0, context_SDP[batch, pre_num, 0])
+                ent_tail[pre_num] = torch.index_select(ent[batch], 0, context_SDP[batch, pre_num, 1])
+
+                ent_head_o = self.tree_LSTM(ent_head[pre_num:pre_num+1, :context_SDP_len[batch, pre_num:pre_num+1, 0]])
+                ent_tail_o = self.tree_LSTM(ent_tail[pre_num:pre_num+1, :context_SDP_len[batch, pre_num:pre_num+1, 1]])
+                print(ent_head_o.size())
+
+                ent_head_output[batch, pre_num] = ent_head_o[0, -1]
+                ent_tail_output[batch, pre_num] = ent_tail_o[0, -1]
+
+        ent = ent_head_output - ent_tail_output#(batch_size,pre_num,128)
+
+        ent = self.linear_ent(ent)#(batch_size,pre_num,97)
 
         predict_re = 0.5 * ent + (1-0.5) * doc.unsqueeze(1).expand(ent.size())
         
-        #print(predict_re[0].size())#(batch_size,re_num,97)
+        #print(predict_re.size())#(batch_size,pre_num,97)
 
         return predict_re
 
@@ -144,7 +152,7 @@ class DepST_RNN(nn.Module):
                 for i in child_num.keys():
                     child[batch, int(i)] /= child_num[i]
 
-        context = torch.cat([context, child] ,dim=-1)
+        context = torch.cat([context, child], dim=-1)
 
         return context
 
@@ -262,17 +270,11 @@ class EncoderLSTM(nn.Module):
         return self.init_hidden[i].expand(-1, bsz, -1).contiguous(), self.init_c[i].expand(-1, bsz, -1).contiguous()
 
     def forward(self, input, input_lengths=None):
-        lengths = torch.tensor(input_lengths)
-        lens, indices = torch.sort(lengths, 0, True)
-        input = input[indices]
-        _, _indices = torch.sort(indices, 0)
-
         bsz, slen = input.size(0), input.size(1)
         output = input
         outputs = []
-        #if input_lengths is not None:
-        #    lens = input_lengths.data.cpu().numpy()
-        lens[lens==0] = 1
+        if input_lengths is not None:
+            lens = input_lengths.data.cpu().numpy()
 
         for i in range(self.nlayers):
             hidden, c = self.get_init(bsz, i)
@@ -293,11 +295,10 @@ class EncoderLSTM(nn.Module):
                 outputs.append(hidden.permute(1, 0, 2).contiguous().view(bsz, -1))
             else:
                 outputs.append(output)
-        for i, output in enumerate(outputs):
-            outputs[i] = output[_indices]
         if self.concat:
             return torch.cat(outputs, dim=-1)
         return outputs[-1]
+
 
 class BiAttention(nn.Module):
     def __init__(self, input_size, dropout):
